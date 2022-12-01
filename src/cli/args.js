660cbf44ba3013bc3fbe95452dd031290f3199c7
Dmitri Gaponenko <dm.gaponenko@gmail.com>
2022-12-02 01:08:35 +0300

    feat: add solution for Command line interface tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  console.log(
+    process.argv
+      .slice(2)
+      .map((arg) => arg.slice(0, 2) === '--' ? `${arg.slice(2)} is` : `${arg},`)
+      .join(' ')
+      .slice(0, -1)
+  );
 };
 
 parseArgs();
