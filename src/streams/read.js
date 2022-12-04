8f1b872528c5463e9c860c253b3e70cddd437442
Dmitri Gaponenko <dm.gaponenko@gmail.com>
2022-12-04 22:16:04 +0300

    feat: add solution for Streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(path.join(__dirname, 'files', 'fileToRead.txt'), 'utf-8');
+  let data = '';
+
+  stream.on('data', chunk => data += chunk);
+  stream.on('end', () => process.stdout.write(data + '\n'));
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const stream = fs.createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'), 'utf-8');
+  process.stdin.pipe(stream);
+  console.log(`Please enter text (press 'ctrl + c' to exit):\n`);
 };
 
 await write();
